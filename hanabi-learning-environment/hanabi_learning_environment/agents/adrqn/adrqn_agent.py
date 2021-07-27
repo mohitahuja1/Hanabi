02d12215d4a4333ef96d417953d83cb4e13691ba
Mohit Ahuja <mohit.mukesh.ahuja@gmail.com>
2021-07-27 21:38:21 +0400

    Change DRQN parameters to reflect DQN/R2D2 implementations.

--- hanabi-learning-environment/hanabi_learning_environment/agents/adrqn/adrqn_agent.py
+++ hanabi-learning-environment/hanabi_learning_environment/agents/adrqn/adrqn_agent.py
@@ -151,6 +151,8 @@ class AdrqnAgent(Agent):
                begin = 1, # debug
                i_episode = 0, # debug
                training_steps = 0, # debug
+               update_period = 4, # debug
+               target_update_period = 500, # debug
                tf_device='/cpu:*',):
 
     # Global variables.
@@ -174,6 +176,8 @@ class AdrqnAgent(Agent):
     self.begin = begin # debug
     self.i_episode = i_episode # debug
     self.training_steps = training_steps # debug
+    self.update_period = update_period # debug
+    self.target_update_period = target_update_period # debug
 
     # ADRQN and ExpBuffer instances
     self.replay_buffer = ExpBuffer(self.replay_buffer_size, self.sample_length)
@@ -270,56 +274,62 @@ class AdrqnAgent(Agent):
         np.array([0]*len(self.last_observation[current_player])), # debug
         done
         ))
-    self.adrqn_target.load_state_dict(self.adrqn.state_dict())
     self.i_episode += 1
 
   def bundle_and_checkpoint(self, checkpoint_dir, iteration_number):
     return None
 
   def _update_network(self):
-    self.eps = self.eps_end + (self.eps_start - self.eps_end) * math.exp((-1*(self.i_episode-self.explore))/self.eps_decay)
 
-    last_actions, last_observations, actions, rewards, observations, dones = self.replay_buffer.sample(self.batch_size)
-    q_values, _ = self.adrqn.forward(last_observations, F.one_hot(last_actions, self.num_actions).float())
-    # print("q_values: ", q_values)
-    # print("q_values shape: ", q_values.shape)
-    # print("last_observations shape: ", last_observations.shape)
-    # print("last_actions: ", last_actions)
-    # print("last_actions shape: ", last_actions.shape)
-    # print("actions shape: ", actions.shape)
-    # print("actions unsqueezed shape: ", actions.unsqueeze(-1).shape)
-    # print("q_values data type: ", q_values.dtype)
-    # print("q_values tensor type: ", q_values.type)
-    # q_values = q_values.type(torch.LongTensor)
-    # actions = actions.type(torch.LongTensor)
-    # print("q_values data type 2: ", q_values.dtype)
-    # print("q_values tensor type 2: ", q_values.type)
-    # print("q_values data type: ", q_values.dtype)fl
-    # q_values = q_values.long()
-    # print("q_values data type: ", q_values.dtype)
-    # print("torch version: ", torch.__version__) 
-    # print("actions: ", actions)
-    # print("actions shape: ", actions.shape)
-    # print("actions[0]: ", actions[0])
-    # print("actions[1]: ", actions[1])
-    # print("actions[2]: ", actions[2])
-    # mx = 0
-    # for e in actions:
-    #     if max(e) > mx:
-    #         mx = max(e)
-    # print("max: ", mx)
-    q_values = torch.gather(q_values, -1, actions.unsqueeze(-1)).squeeze(-1)
-    # q_values = torch.gather(q_values.type(torch.LongTensor), -1, actions.type(torch.LongTensor).unsqueeze(-1)).squeeze(-1).type(torch.LongTensor)
-    # print("here")
-    predicted_q_values, _ = self.adrqn_target.forward(observations, F.one_hot(actions, self.num_actions).float())
-    target_values = rewards + (self.gamma * (1 - dones.float()) * torch.max(predicted_q_values, dim = -1)[0])
+    if self.training_steps % self.update_period == 0:
 
-    #Update network parameters
-    self.optimizer.zero_grad()
+      self.eps = self.eps_end + (self.eps_start - self.eps_end) * math.exp((-1*(self.i_episode-self.explore))/self.eps_decay)
+
+      last_actions, last_observations, actions, rewards, observations, dones = self.replay_buffer.sample(self.batch_size)
+      q_values, _ = self.adrqn.forward(last_observations, F.one_hot(last_actions, self.num_actions).float())
+      # print("q_values: ", q_values)
+      # print("q_values shape: ", q_values.shape)
+      # print("last_observations shape: ", last_observations.shape)
+      # print("last_actions: ", last_actions)
+      # print("last_actions shape: ", last_actions.shape)
+      # print("actions shape: ", actions.shape)
+      # print("actions unsqueezed shape: ", actions.unsqueeze(-1).shape)
+      # print("q_values data type: ", q_values.dtype)
+      # print("q_values tensor type: ", q_values.type)
+      # q_values = q_values.type(torch.LongTensor)
+      # actions = actions.type(torch.LongTensor)
+      # print("q_values data type 2: ", q_values.dtype)
+      # print("q_values tensor type 2: ", q_values.type)
+      # print("q_values data type: ", q_values.dtype)fl
+      # q_values = q_values.long()
+      # print("q_values data type: ", q_values.dtype)
+      # print("torch version: ", torch.__version__) 
+      # print("actions: ", actions)
+      # print("actions shape: ", actions.shape)
+      # print("actions[0]: ", actions[0])
+      # print("actions[1]: ", actions[1])
+      # print("actions[2]: ", actions[2])
+      # mx = 0
+      # for e in actions:
+      #     if max(e) > mx:
+      #         mx = max(e)
+      # print("max: ", mx)
+      q_values = torch.gather(q_values, -1, actions.unsqueeze(-1)).squeeze(-1)
+      # q_values = torch.gather(q_values.type(torch.LongTensor), -1, actions.type(torch.LongTensor).unsqueeze(-1)).squeeze(-1).type(torch.LongTensor)
+      # print("here")
+      predicted_q_values, _ = self.adrqn_target.forward(observations, F.one_hot(actions, self.num_actions).float())
+      target_values = rewards + (self.gamma * (1 - dones.float()) * torch.max(predicted_q_values, dim = -1)[0])
+
+      #Update network parameters
+      self.optimizer.zero_grad()
+      loss = torch.nn.MSELoss()(q_values , target_values.detach())
+      loss.backward()
+      self.optimizer.step()
+
+    if self.training_steps % self.target_update_period == 0:
+      self.adrqn_target.load_state_dict(self.adrqn.state_dict())
+
     self.training_steps += 1
-    loss = torch.nn.MSELoss()(q_values , target_values.detach())
-    loss.backward()
-    self.optimizer.step()
 
   # Convert index to one hot action
   def _idx_to_one_hot(self, idx):
